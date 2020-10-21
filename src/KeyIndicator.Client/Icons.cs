5031e177c56900334382eb954bf948a7573ad2b2
Andrea Tosato <andrea.tosato@hotmail.it>
2020-10-21 21:16:07 +0200

    Passaggio a .NET Core

---
+++ src/KeyIndicator.Client/Icons.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Media.Imaging;
+
+namespace KeyIndicator.Client
+{
+    class Icons
+    {
+        private static Icons instance;
+        public static Icons Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Icons();
+
+                return instance;
+            }
+        }
+
+        public BitmapImage NoIndicators { get; private set; }
+        public BitmapImage Off { get; private set; }
+        public BitmapImage Caps { get; private set; }
+        public BitmapImage CapsNum { get; private set; }
+        public BitmapImage CapsNumScroll { get; private set; }
+        public BitmapImage CapsScroll { get; private set; }
+        public BitmapImage Num { get; private set; }
+        public BitmapImage NumScroll { get; private set; }
+        public BitmapImage Scroll { get; private set; }
+
+        private Icons()
+        {
+            NoIndicators = new BitmapImage(new Uri("pack://application:,,,/Icons/lock-single.ico"));
+            Off = new BitmapImage(new Uri("pack://application:,,,/Icons/lock.ico"));
+            Caps = new BitmapImage(new Uri("pack://application:,,,/Icons/lock-caps.ico"));
+            CapsNum = new BitmapImage(new Uri("pack://application:,,,/Icons/lock-caps-num.ico"));
+            CapsNumScroll = new BitmapImage(new Uri("pack://application:,,,/Icons/lock-caps-num-scroll.ico"));
+            CapsScroll = new BitmapImage(new Uri("pack://application:,,,/Icons/lock-caps-scroll.ico"));
+            Num = new BitmapImage(new Uri("pack://application:,,,/Icons/lock-num.ico"));
+            NumScroll = new BitmapImage(new Uri("pack://application:,,,/Icons/lock-num-scroll.ico"));
+            Scroll = new BitmapImage(new Uri("pack://application:,,,/Icons/lock-scroll.ico"));
+        }
+    }
+}
---
+++ src/KeyIndicator.Client/KeyConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace KeyIndicator.Client
+{
+    public class KeyToVisibilityConverter : IValueConverter
+    {
+        public KeyboardHook.VKeys ShowOn { get; set; }
+        public virtual object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value is KeyboardHook.VKeys && (KeyboardHook.VKeys)value == ShowOn) ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
